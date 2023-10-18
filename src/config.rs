bd3c24be1a0fe29cb8c637b8764d8a81b45d696b
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2023-10-18 06:54:17 -0700

    Create and save config during load if it doesn't exist

--- src/config.rs
+++ src/config.rs
@@ -5,7 +5,7 @@ use serde::{Deserialize, Serialize};
 
 /// Need to figure out how to make a proper config?
 /// quicktype.io can help you by converting json to Rust
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct Config {
     // This is not part of the config, but rather used for
     // at runtime to remember where to save to
@@ -18,12 +18,29 @@ pub struct Config {
 }
 
 impl Config {
+    /// Load a config file
+    /// If path doesn't exist, creates and saves default config
+    /// otherwise loads what's already there
     pub fn load<P: AsRef<Path>>(path: P) -> anyhow::Result<Self> {
-        let data = fs::read_to_string(&path)?;
+        let path = path.as_ref();
+
+        // if path doesn't exist, create default config,
+        // save it, and return it
+        if !path.exists() {
+            let config = Self {
+                path: path.to_owned(),
+                ..Default::default()
+            };
+
+            config.save()?;
+            return Ok(config);
+        }
+
+        let data = fs::read_to_string(path)?;
         let mut config = toml::from_str::<Self>(&data)?;
 
         // set the plugin config path
-        config.path = path.as_ref().to_owned();
+        config.path = path.to_owned();
 
         Ok(config)
     }
