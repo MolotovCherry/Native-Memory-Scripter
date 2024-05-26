06712399971f01fc1d11eec4a65ec88d257a6538
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2024-05-26 07:55:21 -0700

    Re-organize mem file

--- crates/native-memory-scripter/src/modules/mem.rs
+++ crates/native-memory-scripter/src/modules/mem.rs
@@ -47,6 +47,30 @@ pub mod mem {
         }
     }
 
+    #[allow(non_camel_case_types)]
+    struct py_lm_prot_t(lm_prot_t);
+    impl<'a> TryFromBorrowedObject<'a> for py_lm_prot_t {
+        fn try_from_borrowed_object(vm: &VirtualMachine, obj: &'a PyObject) -> PyResult<Self> {
+            let value: u8 = obj.try_int(vm)?.try_to_primitive(vm)?;
+
+            let prot = match value {
+                0b000 => lm_prot_t::LM_PROT_NONE,
+                0b001 => lm_prot_t::LM_PROT_X,
+                0b010 => lm_prot_t::LM_PROT_R,
+                0b100 => lm_prot_t::LM_PROT_W,
+                0b011 => lm_prot_t::LM_PROT_XR,
+                0b101 => lm_prot_t::LM_PROT_XW,
+                0b110 => lm_prot_t::LM_PROT_RW,
+                0b111 => lm_prot_t::LM_PROT_XRW,
+
+                _ => return Err(vm.new_value_error(format!("{value} is not a valid lm_prot_t"))),
+            };
+
+            Ok(Self(prot))
+        }
+    }
+
+    /// An Opaque pointer which can be casted back to the original data type
     #[pyclass(name, no_attr)]
     #[derive(Debug)]
     struct Opaque(NonNull<()>);
@@ -60,46 +84,28 @@ pub mod mem {
             Self(NonNull::new(ptr).unwrap())
         }
 
+        /// SAFETY: No other unique refs can exist anywhere when you call this
         unsafe fn as_ref<T>(&self) -> &T {
             let ptr: *mut T = self.0.as_ptr().cast();
             unsafe { &*ptr }
+        }
+
+        /// SAFETY: No other unique or shared refs can exist anywhere when you call this
+        unsafe fn as_mut<T>(&mut self) -> &mut T {
+            let ptr: *mut T = self.0.as_ptr().cast();
+            unsafe { &mut *ptr }
         }
 
         unsafe fn into<T>(self) -> Box<T> {
             unsafe { Box::from_raw(self.0.as_ptr().cast()) }
         }
 
+        /// SAFETY: There must be no calls to any other functions after this
+        ///         as the inside pointer is no longer valid
         unsafe fn drop<T>(&mut self) {
             unsafe {
                 _ = Box::from_raw(self.0.as_ptr().cast::<T>());
             }
-        }
-    }
-
-    #[allow(non_camel_case_types)]
-    struct py_lm_prot_t(lm_prot_t);
-    impl<'a> TryFromBorrowedObject<'a> for py_lm_prot_t {
-        fn try_from_borrowed_object(vm: &VirtualMachine, obj: &'a PyObject) -> PyResult<Self> {
-            let value: u32 = obj.try_int(vm)?.try_to_primitive(vm)?;
-
-            let prot = match value {
-                0b000 => lm_prot_t::LM_PROT_NONE,
-                0b001 => lm_prot_t::LM_PROT_X,
-                0b010 => lm_prot_t::LM_PROT_R,
-                0b100 => lm_prot_t::LM_PROT_W,
-                0b011 => lm_prot_t::LM_PROT_XR,
-                0b101 => lm_prot_t::LM_PROT_XW,
-                0b110 => lm_prot_t::LM_PROT_RW,
-                0b111 => lm_prot_t::LM_PROT_XRW,
-
-                _ => {
-                    return Err(vm.new_value_error(format!(
-                        "{value} is outside the acceptable range for lm_prot_t"
-                    )))
-                }
-            };
-
-            Ok(Self(prot))
         }
     }
 }
