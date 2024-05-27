9c13e523cbf82677fee85b7fc1def42679789b23
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2024-05-27 11:09:04 -0700

    Clean up unused code

--- crates/native-memory-scripter/src/paths.rs
+++ crates/native-memory-scripter/src/paths.rs
@@ -1,11 +1,9 @@
 use std::{
     ffi::OsString,
-    fs,
     os::windows::prelude::OsStringExt,
     path::{Path, PathBuf},
 };
 
-use directories::BaseDirs;
 use windows::{
     core::HRESULT,
     Win32::{
@@ -16,8 +14,6 @@ use windows::{
 
 #[derive(Debug, thiserror::Error)]
 pub enum PathError {
-    #[error("failed to instantiate BaseDirs")]
-    BaseDirsError,
     #[error("{0} dir not found")]
     NotFound(String),
     #[error("{0}")]
@@ -44,53 +40,6 @@ impl From<HRESULT> for PathError {
     fn from(value: HRESULT) -> Self {
         Self::Windows(value)
     }
-}
-
-/// Get the larian local directory
-/// `C:\Users\<user>\AppData\Local\Larian Studios`
-pub fn get_larian_local_dir() -> Result<PathBuf, PathError> {
-    let local = BaseDirs::new().ok_or(PathError::BaseDirsError)?;
-
-    let mut local = local.data_local_dir().to_owned();
-
-    local.push("Larian Studios");
-    if local.exists() {
-        Ok(local)
-    } else {
-        Err(PathError::NotFound("Larian appdata".into()))
-    }
-}
-
-/// Get the BG3 local directory
-/// `C:\Users\<user>\AppData\Local\Larian Studios\Baldur's Gate 3`
-pub fn get_bg3_local_dir() -> Result<PathBuf, PathError> {
-    let mut local = get_larian_local_dir()?;
-
-    local.push("Baldur's Gate 3");
-
-    if local.exists() {
-        Ok(local)
-    } else {
-        Err(PathError::NotFound("Bg3 appdata".into()))
-    }
-}
-
-/// Get the bg3 plugins directory
-/// `C:\Users\<user>\AppData\Local\Larian Studios\Baldur's Gate 3\Plugins`
-pub fn get_bg3_plugins_dir() -> Result<PathBuf, PathError> {
-    let mut plugins_dir = get_bg3_local_dir()?;
-    plugins_dir.push("Plugins");
-
-    if plugins_dir.exists() {
-        Ok(plugins_dir)
-    } else {
-        Err(PathError::NotFound("BG3 Plugins".into()))
-    }
-}
-
-/// Create a path to `C:\Users\<user>\AppData\Local\Larian Studios\Baldur's Gate 3\Plugins\<filename>`
-pub fn get_plugins_filepath<P: AsRef<Path>>(path: P) -> Result<PathBuf, PathError> {
-    Ok(get_bg3_plugins_dir()?.join(path))
 }
 
 /// Get path to dll `<dll_dir>\myplugin.dll`
@@ -121,33 +70,10 @@ pub fn get_dll_dir(module: HINSTANCE) -> Result<PathBuf, PathError> {
     Ok(dll_folder)
 }
 
-/// Get path to `<dll_dir>\logs\`
-/// Also creates `logs` dir if it doesn't exist
-pub fn get_dll_logs_dir(module: HINSTANCE) -> Result<PathBuf, PathError> {
-    let mut logs_dir = get_dll_dir(module)?;
-    logs_dir.push("logs");
-
-    if !logs_dir.exists() {
-        fs::create_dir(&logs_dir)?;
-    }
-
-    Ok(logs_dir)
-}
-
 /// Get path to `<dll_dir>\<filename>`
 pub fn get_dll_dir_filepath<P: AsRef<Path>>(
     module: HINSTANCE,
     path: P,
 ) -> Result<PathBuf, PathError> {
     Ok(get_dll_dir(module)?.join(path))
-}
-
-/// Get path to `<dll_dir>\logs\<filename>`
-/// Also creates `logs` dir if it doesn't exist
-pub fn get_dll_logs_filepath<P: AsRef<Path>>(
-    module: HINSTANCE,
-    path: P,
-) -> Result<PathBuf, PathError> {
-    let logs_dir = get_dll_logs_dir(module)?;
-    Ok(logs_dir.join(path))
 }
