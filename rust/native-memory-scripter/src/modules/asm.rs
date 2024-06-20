535266c646542cbf4774a2e9e97c52d080e4cf24
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2024-06-20 09:36:00 -0700

    Combine into 1 big disassembly function

--- rust/native-memory-scripter/src/modules/asm.rs
+++ rust/native-memory-scripter/src/modules/asm.rs
@@ -77,6 +77,12 @@ pub mod asm {
     /// unsafe fn
     #[pyfunction]
     fn disassemble(args: FuncArgs, vm: &VirtualMachine) -> PyResult<PyObjectRef> {
+        // disassemble bytes:
+        // Address
+        // Address, runtime_addr(Address)
+        // Address, runtime_addr(Address), count(usize)
+
+        // disassemble address:
         // Address
         // Address, size (usize), runtime_addr(Address)
         // Address, size (usize), runtime_addr(Address), count(usize)
@@ -86,181 +92,178 @@ pub mod asm {
             .first()
             .map(|s| s.try_to_value::<Address>(vm))
             .transpose()?
-            .ok_or_else(|| vm.new_runtime_error("address argument not found".to_owned()))?;
+            .ok_or_else(|| vm.new_runtime_error("address argument not found".to_owned()));
 
-        let obj = match args.args.len() {
-            1 => {
-                let res = unsafe { mem::asm::disassemble(address as *const _) };
-                let res = res.map_err(|e| vm.new_runtime_error(format!("{e}")))?;
+        if let Ok(address) = address {
+            let obj = match args.args.len() {
+                1 => {
+                    let res = unsafe { mem::asm::disassemble(address as *const _) };
+                    let res = res.map_err(|e| vm.new_runtime_error(format!("{e}")))?;
 
-                PyInst(res).to_pyobject(vm)
-            }
+                    PyInst(res).to_pyobject(vm)
+                }
 
-            3 => {
-                let size = args
-                    .args
-                    .get(1)
-                    .cloned()
-                    .map(|i| i.try_into_value::<usize>(vm))
-                    .transpose()?
-                    .unwrap();
+                3 => {
+                    let size = args
+                        .args
+                        .get(1)
+                        .cloned()
+                        .map(|i| i.try_into_value::<usize>(vm))
+                        .transpose()?
+                        .unwrap();
 
-                let runtime_addr = args
-                    .args
-                    .get(2)
-                    .cloned()
-                    .map(|i| i.try_into_value::<Address>(vm))
-                    .transpose()?
-                    .unwrap();
+                    let runtime_addr = args
+                        .args
+                        .get(2)
+                        .cloned()
+                        .map(|i| i.try_into_value::<Address>(vm))
+                        .transpose()?
+                        .unwrap();
 
-                let res =
-                    unsafe { mem::asm::disassemble_ex(address as *const _, size, runtime_addr) };
+                    let res = unsafe {
+                        mem::asm::disassemble_ex(address as *const _, size, runtime_addr)
+                    };
 
-                let res = res.map_err(|e| vm.new_runtime_error(format!("{e}")))?;
-                let insts = res
-                    .into_iter()
-                    .map(|d| PyInst(d).to_pyobject(vm))
-                    .collect::<Vec<_>>();
+                    let res = res.map_err(|e| vm.new_runtime_error(format!("{e}")))?;
+                    let insts = res
+                        .into_iter()
+                        .map(|d| PyInst(d).to_pyobject(vm))
+                        .collect::<Vec<_>>();
 
-                insts.to_pyobject(vm)
-            }
+                    insts.to_pyobject(vm)
+                }
 
-            4 => {
-                let size = args
-                    .args
-                    .get(1)
-                    .cloned()
-                    .map(|i| i.try_into_value::<usize>(vm))
-                    .transpose()?
-                    .unwrap();
+                4 => {
+                    let size = args
+                        .args
+                        .get(1)
+                        .cloned()
+                        .map(|i| i.try_into_value::<usize>(vm))
+                        .transpose()?
+                        .unwrap();
 
-                let runtime_addr = args
-                    .args
-                    .get(2)
-                    .cloned()
-                    .map(|i| i.try_into_value::<Address>(vm))
-                    .transpose()?
-                    .unwrap();
+                    let runtime_addr = args
+                        .args
+                        .get(2)
+                        .cloned()
+                        .map(|i| i.try_into_value::<Address>(vm))
+                        .transpose()?
+                        .unwrap();
 
-                let inst_count = args
-                    .args
-                    .get(3)
-                    .cloned()
-                    .map(|i| i.try_into_value::<usize>(vm))
-                    .transpose()?
-                    .unwrap();
+                    let inst_count = args
+                        .args
+                        .get(3)
+                        .cloned()
+                        .map(|i| i.try_into_value::<usize>(vm))
+                        .transpose()?
+                        .unwrap();
 
-                let res = unsafe {
-                    mem::asm::disassemble_ex_count(
-                        address as *const _,
-                        size,
-                        runtime_addr,
-                        inst_count,
-                    )
-                };
+                    let res = unsafe {
+                        mem::asm::disassemble_ex_count(
+                            address as *const _,
+                            size,
+                            runtime_addr,
+                            inst_count,
+                        )
+                    };
 
-                let res = res.map_err(|e| vm.new_runtime_error(format!("{e}")))?;
-                let insts = res
-                    .into_iter()
-                    .map(|d| PyInst(d).to_pyobject(vm))
-                    .collect::<Vec<_>>();
+                    let res = res.map_err(|e| vm.new_runtime_error(format!("{e}")))?;
+                    let insts = res
+                        .into_iter()
+                        .map(|d| PyInst(d).to_pyobject(vm))
+                        .collect::<Vec<_>>();
 
-                insts.to_pyobject(vm)
-            }
+                    insts.to_pyobject(vm)
+                }
 
-            _ => {
-                return Err(vm.new_runtime_error(format!(
-                    "expected 1,3,or 4 args, found {}",
-                    args.args.len()
-                )))
-            }
-        };
+                _ => {
+                    return Err(vm.new_runtime_error(format!(
+                        "expected 1,3,or 4 args, found {}",
+                        args.args.len()
+                    )))
+                }
+            };
 
-        Ok(obj)
-    }
+            Ok(obj)
+        } else {
+            let bytes = args
+                .args
+                .first()
+                .cloned()
+                .map(|s| s.try_into_value::<Vec<u8>>(vm))
+                .transpose()?
+                .ok_or_else(|| vm.new_runtime_error("address argument not found".to_owned()))?;
 
-    #[pyfunction]
-    fn disassemble_bytes(args: FuncArgs, vm: &VirtualMachine) -> PyResult<PyObjectRef> {
-        // Address
-        // Address, runtime_addr(Address)
-        // Address, runtime_addr(Address), count(usize)
+            let obj = match args.args.len() {
+                1 => {
+                    let res = mem::asm::disassemble_bytes(&bytes);
+                    let res = res.map_err(|e| vm.new_runtime_error(format!("{e}")))?;
+                    let res = res
+                        .into_iter()
+                        .map(|i| PyInst(i).to_pyobject(vm))
+                        .collect::<Vec<_>>();
 
-        let bytes = args
-            .args
-            .first()
-            .cloned()
-            .map(|s| s.try_into_value::<Vec<u8>>(vm))
-            .transpose()?
-            .ok_or_else(|| vm.new_runtime_error("address argument not found".to_owned()))?;
+                    res.to_pyobject(vm)
+                }
 
-        let obj = match args.args.len() {
-            1 => {
-                let res = mem::asm::disassemble_bytes(&bytes);
-                let res = res.map_err(|e| vm.new_runtime_error(format!("{e}")))?;
-                let res = res
-                    .into_iter()
-                    .map(|i| PyInst(i).to_pyobject(vm))
-                    .collect::<Vec<_>>();
+                2 => {
+                    let runtime_addr = args
+                        .args
+                        .get(1)
+                        .cloned()
+                        .map(|i| i.try_into_value::<usize>(vm))
+                        .transpose()?
+                        .unwrap();
 
-                res.to_pyobject(vm)
-            }
+                    let res = mem::asm::disassemble_bytes_ex(&bytes, runtime_addr);
+                    let res = res.map_err(|e| vm.new_runtime_error(format!("{e}")))?;
+                    let res = res
+                        .into_iter()
+                        .map(|i| PyInst(i).to_pyobject(vm))
+                        .collect::<Vec<_>>();
 
-            2 => {
-                let runtime_addr = args
-                    .args
-                    .get(1)
-                    .cloned()
-                    .map(|i| i.try_into_value::<usize>(vm))
-                    .transpose()?
-                    .unwrap();
+                    res.to_pyobject(vm)
+                }
 
-                let res = mem::asm::disassemble_bytes_ex(&bytes, runtime_addr);
-                let res = res.map_err(|e| vm.new_runtime_error(format!("{e}")))?;
-                let res = res
-                    .into_iter()
-                    .map(|i| PyInst(i).to_pyobject(vm))
-                    .collect::<Vec<_>>();
+                3 => {
+                    let runtime_addr = args
+                        .args
+                        .get(1)
+                        .cloned()
+                        .map(|i| i.try_into_value::<Address>(vm))
+                        .transpose()?
+                        .unwrap();
 
-                res.to_pyobject(vm)
-            }
+                    let inst_count = args
+                        .args
+                        .get(2)
+                        .cloned()
+                        .map(|i| i.try_into_value::<usize>(vm))
+                        .transpose()?
+                        .unwrap();
 
-            3 => {
-                let runtime_addr = args
-                    .args
-                    .get(1)
-                    .cloned()
-                    .map(|i| i.try_into_value::<Address>(vm))
-                    .transpose()?
-                    .unwrap();
+                    let res =
+                        mem::asm::disassemble_bytes_ex_count(&bytes, runtime_addr, inst_count);
 
-                let inst_count = args
-                    .args
-                    .get(2)
-                    .cloned()
-                    .map(|i| i.try_into_value::<usize>(vm))
-                    .transpose()?
-                    .unwrap();
+                    let res = res.map_err(|e| vm.new_runtime_error(format!("{e}")))?;
+                    let insts = res
+                        .into_iter()
+                        .map(|d| PyInst(d).to_pyobject(vm))
+                        .collect::<Vec<_>>();
 
-                let res = mem::asm::disassemble_bytes_ex_count(&bytes, runtime_addr, inst_count);
+                    insts.to_pyobject(vm)
+                }
 
-                let res = res.map_err(|e| vm.new_runtime_error(format!("{e}")))?;
-                let insts = res
-                    .into_iter()
-                    .map(|d| PyInst(d).to_pyobject(vm))
-                    .collect::<Vec<_>>();
+                _ => {
+                    return Err(vm.new_runtime_error(format!(
+                        "expected 1, 2 or 3 args, found {}",
+                        args.args.len()
+                    )))
+                }
+            };
 
-                insts.to_pyobject(vm)
-            }
-
-            _ => {
-                return Err(vm.new_runtime_error(format!(
-                    "expected 1, 2 or 3 args, found {}",
-                    args.args.len()
-                )))
-            }
-        };
-
-        Ok(obj)
+            Ok(obj)
+        }
     }
 
     #[pyattr]
